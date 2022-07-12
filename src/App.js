ac7eea9be96d2d9f3e782421586474e729d85922
sikumiku <sigridaasma@gmail.com>
2022-07-13 01:50:46 +0300

    deploy

--- src/App.js
+++ src/App.js
@@ -133,7 +133,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    fetch('http://localhost:3000/imageurl', {
+    fetch('https://aqueous-sierra-18232.herokuapp.com/imageurl', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
@@ -143,7 +143,7 @@ class App extends Component {
     .then(response => response.json())
     .then(response => {
       if (response) {
-        fetch('http://localhost:3000/image', {
+        fetch('https://aqueous-sierra-18232.herokuapp.com/image', {
           method: 'put',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
